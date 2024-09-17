81ffa36119f13780b291557ab9144b8d25276d24
MildredMtz <mildred27@ciencias.unam.mx>
2024-09-17 14:09:15 -0600

    Algoritmo Broadcast

--- Practicas/Practica2/src/NodoBroadcast.py
+++ Practicas/Practica2/src/NodoBroadcast.py
@@ -19,5 +19,24 @@ class NodoBroadcast(Nodo):
     def broadcast(self, env):
         ''' Algoritmo de Broadcast. Desde el nodo distinguido (0)
             vamos a enviar un mensaje a todos los demás nodos.'''
-        # Tú código aquí
-        pass
+        if self.mensaje and self.id_nodo == 0: #  Si es el nodo distinguido (0)
+            # mensaje que se quiere difundir
+            print(f'tiempo {env.now}: Nodo {self.id_nodo} envia mensaje "{self.mensaje}"' ) 
+            # linea 5 del codigo
+            for vecino in self.vecinos:
+                self.canal_salida.envia((self.id_nodo, self.mensaje), vecino)
+
+        while True:
+            # el nodo espera el mensaje del nodo_emisor 
+            mensaje = yield self.canal_entrada.get()
+            nodo_emisor, contenido = mensaje # recibe el contenido del mensaje y quien lo envio 
+            if nodo_emisor not in self.recibido:
+                self.recibido.add(nodo_emisor) # si el nodo_emisor ya recibio el mensaje se guarda para que no lo vuelva a recibir 
+                print(f'Tiempo {env.now}: Nodo {self.id_nodo} ya recibió el mensaje "{contenido}" del Nodo {nodo_emisor}')
+
+                # Reenvía el mensaje a sus vecinos excepto al nodo_emisor
+                for vecino in self.vecinos:
+                    if vecino != nodo_emisor:  # Exentamos al nodo_emisor para evitar un duplicado de mensajes en el mismo nodo
+                        self.canal_salida.envia((self.id_nodo, contenido), vecino)
+            
+            yield env.timeout(TICK)  # Usamos yield env.timeout para el tiempo de procesamiento
